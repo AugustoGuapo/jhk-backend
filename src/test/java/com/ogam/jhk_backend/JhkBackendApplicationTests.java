fb45de531f3b6900d99261c6dfb3552f5ff707b1
Augusto Diaz <adiazpolo17@correo.unicordoba.edu.co>
2024-09-21 21:10:20 -0500

    primer commit de todos

---
+++ src/test/java/com/ogam/jhk_backend/JhkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogam.jhk_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
